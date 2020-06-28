7c0152b10c45e8175f5d04999f368b879f7b100b
ArKc1009 <arkc1009@gmail.com>
2020-06-28 21:44:57 +0900

    python ignore

--- Python/11173.py
+++
@@ -1,4 +0,0 @@
-N=str(bin(int(input())))
-N=N[2:]
-N=N[::-1]
-print(int(N, 2))
--- Python/3447.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-asd = sys.stdin.read()
-ans=asd.replace("BUG", "")
-
-while ans.find('BUG') !=-1 :
-    ans = ans.replace("BUG", "")
-
-print(ans)
